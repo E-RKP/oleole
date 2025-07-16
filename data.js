8a5eac47dc750c4083d26df60cf9eb70ef18a84a
E-RKP <edobackup111@gmail.com>
2025-07-16 17:28:34 +0200

    version 3

--- data.js
+++ data.js
@@ -44,3 +44,52 @@ azzera.addEventListener('click',()=>{
   numerino.textContent = numpic
   numerone.textContent = numbig
 })
+
+let lista = [
+
+  
+  {
+    name: 'Paolo',
+    età: 22,
+    nickname: 'Gigi',
+    pa: '2u2943u9'
+  },
+
+  {
+    name: 'Mr mario',
+    età: 28,
+    nickname: 'loki',
+    pa: 'vevev'
+  },
+
+  {
+    name: 'maria',
+    età: 19,
+    nickname: 'Gig',
+    pa: 'ncwi/8j'
+  },
+
+  {
+    name: 'lucia',
+    età: 22,
+    nickname: 'wolf',
+    pa: '0987h'
+  },
+
+  {
+    name: 'Davide',
+    età: 33,
+    nickname: 'dedo',
+    pa: '2u2943umcwo,,9'
+  },
+
+
+  signIn = function(okk){
+    for (i=0; i<okk.length;i++){
+      console.log(okk[i].name)
+    }
+  }
+]
+
+signIn(lista);
+
